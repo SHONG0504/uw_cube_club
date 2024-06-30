5ee4d9140eb68b208133e8a2cb4d8b8809ce93ec
SHONG0504 <seungminhong2002@gmail.com>
2024-06-29 22:11:23 -0400

    Im a dumdum

--- members/profile.php
+++ members/profile.php
@@ -14,6 +14,23 @@ $wca_id = strtoupper($_GET["wca_id"]);
 
 $sql = "SELECT name, program, level, year, wca FROM members WHERE wca='".$wca_id."'";
 $result = $conn->query($sql);
-print_r($result);
-print($sql);
+// print_r($result);
+// print($sql);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/index.css" type="text/css"/>
+  <title>Members</title>
+</head>
+
+<body>
+    <?php
+    $row = $result->fetch_assoc();
+    echo($row["name"]." ".$row["program"]);
+    ?>
+</body>
+</html>
